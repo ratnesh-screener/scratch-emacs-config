2c7b3136ab6287cd19137da02366db0c30dfb7d5
ratnesh-screener <ratneshrastogi1994@gmail.com>
2025-12-26 23:44:07 +0530

    Updated Emacs config

--- init.el
+++ init.el
@@ -18,25 +18,70 @@
 (doom-modeline-mode 1)
 (global-display-line-numbers-mode 1)
 
-
-(add-to-list 'initial-frame-alist '(fullscreen . maximized))
-
 (set-face-attribute 'default nil :height 160)
 
-(use-package golden-ratio :ensure t :diminish golden-ratio-mode :init (golden-ratio-mode 1))
 
-(add-hook 'after-init-hook 'global-company-mode)   '("0c83e0b50946e39e237769ad368a08f2cd1c854ccbcd1a01d39fdce4d6f86478"
-     "0325a6b5eea7e5febae709dab35ec8648908af12cf2d2b569bedc8da0a3a81c1"
-     "e13beeb34b932f309fb2c360a04a460821ca99fe58f69e65557d6c1b10ba18c7"
+(add-hook 'python-mode-hook #'tree-sitter-hl-mode)
+
+(use-package vertico
+  :init
+  (vertico-mode))
+
+(use-package vertico-posframe
+  :ensure t
+  :init
+  (vertico-posframe-mode)
+  :custom
+  (vertico-posframe-parameters
+   '((left-fringe . 8)
+     (right-fringe . 8))))
+
+;; Persist history over Emacs restarts. Vertico sorts by history position.
+(use-package savehist
+  :init
+  (savehist-mode))
+
+;; Emacs minibuffer configurations.
+(use-package emacs
+  :custom
+  ;; Enable context menu. `vertico-multiform-mode' adds a menu in the minibuffer
+  ;; to switch display modes.
+  (context-menu-mode t)
+  ;; Support opening new minibuffers from inside existing minibuffers.
+  (enable-recursive-minibuffers t)
+  ;; Hide commands in M-x which do not work in the current mode.  Vertico
+  ;; commands are hidden in normal buffers. This setting is useful beyond
+  ;; Vertico.
+  (read-extended-command-predicate #'command-completion-default-include-p)
+  ;; Do not allow the cursor in the minibuffer prompt
+  (minibuffer-prompt-properties
+   '(read-only t cursor-intangible t face minibuffer-prompt)))
+
+;; Optionally use the `orderless' completion style.
+(use-package orderless
+  :custom
+  ;; Configure a custom style dispatcher (see the Consult wiki)
+  ;; (orderless-style-dispatchers '(+orderless-consult-dispatch orderless-affix-dispatch))
+  ;; (orderless-component-separator #'orderless-escapable-split-on-space)
+  (completion-styles '(orderless basic))
+  (completion-category-overrides '((file (styles partial-completion))))
+  (completion-category-defaults nil) ;; Disable defaults, use our settings
+  (completion-pcm-leading-wildcard t)) ;; Emacs 31: partial-completion behaves like substring
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-enabled-themes '(doom-challenger-deep))
+ '(custom-safe-themes
+   '("13096a9a6e75c7330c1bc500f30a8f4407bd618431c94aeab55c9855731a95e1"
      default))
  '(highlight-indent-guides-method 'character)
- '(lsp-ui-doc-enable t)
  '(package-selected-packages
    '(all-the-icons company consult doom-modeline doom-themes golden-ratio
-                   gruber-darker-theme highlight-indent-guides
-                   json-mode lsp-ui orderless page-break-lines popup
-                   projectile tree-sitter tree-sitter-langs vertico
-                   vertico-posframe)))
+                   highlight-indent-guides json-mode lsp-ui orderless
+                   page-break-lines popup projectile tree-sitter
+                   tree-sitter-langs vertico-posframe)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
